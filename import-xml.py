e63022e8b811bda8c77e76f870866a2493ec9155
nils <nils@shkoo.com>
2011-07-05 21:08:30 -0700

    allow ticket import from a url, and allow it to update instead of replace the ticket list

--- import-xml.py
+++ import-xml.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python
+#!/opt/local/bin/python2.6
+
+TICKET_XML_URL="http://example.com/brt_ticket_export.xml"
 
 from xml.dom.minidom import parse
+import urllib
 import sqlite3
 import os
 from datetime import datetime, time
@@ -13,12 +16,11 @@ def fromxmldate(datestr):
     # print datestr, " -> ", newdate.isoformat()
     return newdate
 
-os.remove("ticketdb")
-ticketxml = parse("burn_scan/brt_ticket_export.xml")
 sqlconn = sqlite3.connect("ticketdb")
 cursor = sqlconn.cursor()
+
 cursor.execute('''
-create table tier (
+create table if not exists tier (
    tier_id char(1) not null,
    label varchar(50) not null,
    primary key (tier_id)
@@ -26,7 +28,7 @@ create table tier (
 ''')
 
 cursor.execute('''
-create table ticket (
+create table if not exists  ticket (
    barcode varchar(100) not null,
    tier_id char(1) not null,
    code int not null,
@@ -40,7 +42,7 @@ create table ticket (
 ''')
 
 cursor.execute('''
-create table ticketlog (
+create table if not exists ticketlog (
   barcode varchar(100) not null,
   greeter varchar(100) not null,
   message text,
@@ -48,9 +50,12 @@ create table ticketlog (
 ''')
 
 cursor.execute('''
-create index ticketlogidx on ticketlog (barcode)
+create index if not exists ticketlogidx on ticketlog (barcode)
 ''')
 
+
+urlreader = urllib.urlopen(TICKET_XML_URL)
+ticketxml = parse(urlreader)
 
 tiers = ticketxml.getElementsByTagName("tier")
 for tier in tiers:
@@ -58,7 +63,7 @@ for tier in tiers:
     tier_id = tier.getAttribute("code")
     for tiernode in tier.childNodes:
         if tiernode.nodeName == "label":
-            cursor.execute("insert into tier (tier_id, label) values (?, ?)", (tier_id, tiernode.childNodes[0].wholeText))
+            cursor.execute("insert or ignore into tier (tier_id, label) values (?, ?)", (tier_id, tiernode.childNodes[0].wholeText))
         if tiernode.nodeName != "tickets":
             continue
 
@@ -72,9 +77,10 @@ for tier in tiers:
             barcode = "%s%05i%s" % (tier_id, int(number), attribs["code"])
             
             cursor.execute('''
-insert into ticket (barcode, tier_id, code, number, user_email, assigned_name, purchase_date, entry_at)
+insert or ignore into ticket (barcode, tier_id, code, number, user_email, assigned_name, purchase_date, entry_at)
 values (?, ?, ?, ?, ?, ?, ?, null)''',
                            (barcode, tier_id, attribs["code"], number, attribs["user_email"], attribs["assigned_name"], fromxmldate(attribs["purchase_date"])))
+
 
 sqlconn.commit()
 sqlconn.close()
